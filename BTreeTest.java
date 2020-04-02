22d975bf6dd923ae0a34448599299acb40c0aa98
Alfredo Perez <50719461+alfredopzr@users.noreply.github.com>
2020-04-02 18:45:51 -0500

    Update BTreeTest.java

--- BTreeTest.java
+++ BTreeTest.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BTreeTest
 {
@@ -10,9 +11,22 @@ public class BTreeTest
         /** make object of ExpressionTree **/
         BTree et = new BTree();
  
-        System.out.println("\nEnter equation in prefix form");
-        et.buildTree(scan.next());
- 
+        System.out.println("\nEnter equation: ");
+        
+        
+        String input = scan.next();
+        
+		char firstCharacter = input.charAt(0);
+
+		
+		switch (firstCharacter) {
+		case '!': et.buildTreePre(input.substring(1));
+		break;
+		case '@': et.buildTreePost(input.substring(1));
+		break;
+		default: System.out.println("Neither ");
+	}
+        
         System.out.print("\nPrefix  : ");
         et.prefix();
         System.out.print("\n\nInfix   : ");
